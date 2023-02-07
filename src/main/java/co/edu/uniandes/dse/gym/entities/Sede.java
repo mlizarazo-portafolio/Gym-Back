c357b713a41d09449c671dd89ffea7778687fb8d
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-02-07 09:22:33 -0500

    jp.hernandezt1@uniandes.edu.co - Creación Clase Sede.Java

---
+++ src/main/java/co/edu/uniandes/dse/gym/entities/Sede.java
@@ -0,0 +1,14 @@
+package co.edu.uniandes.dse.gym.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+
+public class Sede {
+
+    private String nombre;
+    private String direccion;
+    private Long telefono;
+    private String portafolio;
+
+}
