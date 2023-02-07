8d7733cbace613b4922df26ef2e4e4f3b7ca2596
SantiTV <s.tenjov@uniandes.edu.co>
2023-02-07 09:23:04 -0500

    Primer commit

---
+++ src/main/java/co/edu/uniandes/dse/gym/entities/PlanEntrenamientoEntity.java
@@ -0,0 +1,17 @@
+package co.edu.uniandes.dse.gym.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class PlanEntrenamientoEntity {
+
+    private String objetivoBasico;
+    private String nombre;
+    private String descripcion;
+    private String dirrecion;
+    private String coordenada;
+    private Integer duracion;
+    private Integer costo;
+    
+
+}
