33e2f081c15e19c89c58bfc4d1506c770fb7e7e0
CarlosMunoz0 <c.munoza@uniandes.edu.co>
2023-02-08 09:15:26 -0500

    relación actividad entrenador

--- src/main/java/co/edu/uniandes/dse/gym/entities/ActividadEntity.java
+++ src/main/java/co/edu/uniandes/dse/gym/entities/ActividadEntity.java
@@ -3,12 +3,21 @@ package co.edu.uniandes.dse.gym.entities;
 import java.sql.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
+
 public class ActividadEntity extends BaseEntity{
     private String nombre;
     private Date fecha;
     private String ubicacion;
     private String cupos;
-    
+    @OneToOne
+    private EntrenadorEntity entrenador; 
+
 }
