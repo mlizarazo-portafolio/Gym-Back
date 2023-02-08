aed388d43d7a0da2c1635e07db95d3cce8a953d9
Maria <m.lizarazop@uniandes.edu.co>
2023-02-08 09:28:44 -0500

    Update AtletaEntity.java

--- src/main/java/co/edu/uniandes/dse/gym/entities/AtletaEntity.java
+++ src/main/java/co/edu/uniandes/dse/gym/entities/AtletaEntity.java
@@ -2,6 +2,10 @@ package co.edu.uniandes.dse.gym.entities;
 
 import javax.persistence.Entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.sql.Array;
+
 @Entity
 
 public class AtletaEntity extends BaseEntity {
@@ -16,6 +20,81 @@ public class AtletaEntity extends BaseEntity {
     private String correo;
     private String documento;
     private String fechaNacimiento;
-    private String planAdquirido;
+    private PlanEntrenamientoEntity planAdquirido;
+    private SedeEntity sede;
+    // private DeportologoEntity deportologo;
+    private ArrayList<ActividadEntity> clasesInscritas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public void setTipoSangre(String tipoSangre) {
+        this.tipoSangre = tipoSangre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 
 }
