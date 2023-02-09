4ec6b9ed612c931aad2b4195feb192b724720076
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-02-09 17:43:44 -0500

    Sede.java

--- src/main/java/co/edu/uniandes/dse/gym/entities/SedeEntity.java
+++ src/main/java/co/edu/uniandes/dse/gym/entities/SedeEntity.java
@@ -1,45 +1,23 @@
 package co.edu.uniandes.dse.gym.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+// import java.util.ArrayList;
+// import java.sql.Array;
 
+@Getter
+@Setter
 @Entity
 
 public class SedeEntity extends BaseEntity {
 
+    private PlanEntrenamientoEntity planEntrenamiento;
+    @OneToMany(mappedBy = "planEntrenamiento")
+
     private String nombre;
     private String direccion;
     private String telefono;
-
-    public SedeEntity(){};
-
-    public SedeEntity(String nombre, String direccion, String telefono) {
-        this.nombre = nombre;
-        this.direccion = direccion;
-        this.telefono = telefono;
-    }
-
-    public String getNombre() {
-        return this.nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return this.direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return this.telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 
 }
