f874d62746fb7452849fc4f3ff1d8b66a7668541
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-02-12 13:08:51 -0500

    Repositories Sede y Servicio

--- src/main/java/co/edu/uniandes/dse/gym/repositories/ServicioRepository.java
+++ src/main/java/co/edu/uniandes/dse/gym/repositories/ServicioRepository.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import co.edu.uniandes.dse.gym.entities.SedeEntity;
+import co.edu.uniandes.dse.gym.entities.ServicioEntity;
 
 @Repository
-public interface SedeRepository extends JpaRepository<SedeEntity, Long>{
+public interface ServicioRepository extends JpaRepository<ServicioEntity, Long>{
 
-    List<SedeEntity> findByNombre(String nombre);
+    List<ServicioEntity> findByNombre(String nombre);
     
 }
