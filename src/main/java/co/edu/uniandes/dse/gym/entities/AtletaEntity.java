42884cebeca20a6ebe037dc4ebb8d68ed4b6fdca
Maria <m.lizarazop@uniandes.edu.co>
2023-02-13 22:42:14 -0500

    atleta entidad y relaciones

--- src/main/java/co/edu/uniandes/dse/gym/entities/AtletaEntity.java
+++ src/main/java/co/edu/uniandes/dse/gym/entities/AtletaEntity.java
@@ -1,30 +1,35 @@
 package co.edu.uniandes.dse.gym.entities;
 
 import javax.persistence.Entity;
-
-import lombok.Getter;
-import lombok.Setter;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.sql.Array;
 import java.sql.Date;
 
-@Getter
-@Setter
 @Entity
+
 public class AtletaEntity extends BaseEntity {
     private String nombre;
     private String login;
     private String tipoSangre;
     private String direccion;
     private Date fechaNacimiento;
-    private Double altura;
-    private Double peso;
-
+    private Integer altura;
+    private Integer peso;
     // private PlanEntrenamientoEntity planEntrenamiento;
     // private SedeEntity sedeGimnasio;
     // private DeportologoEntity deportologo;
     // private ArrayList<ActividadEntity> clasesInscritas;
+    @ManyToOne
+    public SedeEntity sede;
+    @ManyToOne
+    public PlanEntrenamientoEntity plan;
+    @ManyToMany(mappedBy = "atletasInscritos")
+    public ArrayList<ActividadEntity> actividadesInscritas;
+    // @ManyToOne
+    // public DeportologoEntity deportologo;
 
 }
