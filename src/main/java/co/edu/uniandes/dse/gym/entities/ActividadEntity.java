8978031ec616cc6dfb3260f4d72d7c0d3764594a
Maria <m.lizarazop@uniandes.edu.co>
2023-02-13 23:07:52 -0500

    arreglo actividad

--- src/main/java/co/edu/uniandes/dse/gym/entities/ActividadEntity.java
+++ src/main/java/co/edu/uniandes/dse/gym/entities/ActividadEntity.java
@@ -1,8 +1,10 @@
 package co.edu.uniandes.dse.gym.entities;
 
 import java.sql.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 import lombok.Getter;
@@ -12,16 +14,19 @@ import lombok.Setter;
 @Setter
 @Entity
 
-public class ActividadEntity extends BaseEntity{
+public class ActividadEntity extends BaseEntity {
     private String nombre;
     private Date fecha;
     private String ubicacion;
     private String cupos;
-    
+
     @OneToOne(mappedBy = "actividad")
-    private EntrenadorEntity entrenador; 
+    private EntrenadorEntity entrenador;
 
     @OneToOne(mappedBy = "actividad")
     private RestriccionEntity restriccion;
+
+    @ManyToMany
+    private List<AtletaEntity> atletasInscritos;
 
 }
