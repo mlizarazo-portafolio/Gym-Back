abd0bd3eaa6b011fcc8600b76c9b7510ed7beb49
CarlosMunoz0 <c.munoza@uniandes.edu.co>
2023-02-15 09:32:53 -0500

    Logica Restriccion y Actividad

--- src/main/java/co/edu/uniandes/dse/gym/services/ActividadService.java
+++ src/main/java/co/edu/uniandes/dse/gym/services/ActividadService.java
@@ -1,8 +1,49 @@
 package co.edu.uniandes.dse.gym.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import co.edu.uniandes.dse.gym.repositories.ActividadRepository;
+import org.springframework.transaction.annotation.Transactional;
+import co.edu.uniandes.dse.gym.entities.ActividadEntity;
+import co.edu.uniandes.dse.gym.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.gym.exceptions.IllegalOperationException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ActividadService {
     
+    @Autowired
+    ActividadRepository actividadRepository;
+
+
+
+    @Transactional
+    public List<ActividadEntity> getActividades(){
+
+        return actividadRepository.findAll();
+    }
+
+    @Transactional
+    public ActividadEntity getActividad(Long actividadId) throws EntityNotFoundException
+    {
+       
+
+        Optional<ActividadEntity> actividadEntity = actividadRepository.findById(actividadId);
+
+        if(actividadEntity.isEmpty())
+            throw new EntityNotFoundException("ACTIVIDAD_NOT_FOUND");
+        
+     
+        return actividadEntity.get(); 
+    }
+
+    @Transactional
+    public ActividadEntity createActividad(ActividadEntity actividadEntity) throws IllegalOperationException {
+        
+        return actividadRepository.save(actividadEntity);
+    }
+
+
 }
--- src/main/java/co/edu/uniandes/dse/gym/services/RestriccionService.java
+++ src/main/java/co/edu/uniandes/dse/gym/services/RestriccionService.java
@@ -1,8 +1,50 @@
 package co.edu.uniandes.dse.gym.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.uniandes.dse.gym.entities.RestriccionEntity;
+import co.edu.uniandes.dse.gym.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.gym.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.gym.repositories.RestriccionRepository;
 
 @Service
-public class RestriccionService {
+public class RestriccionService{
     
+    @Autowired
+    RestriccionRepository restriccionRepository;
+
+
+
+    @Transactional
+    public List<RestriccionEntity> getRestricciones(){
+
+        return restriccionRepository.findAll();
+    }
+
+    @Transactional
+    public RestriccionEntity getRestriccion(Long restriccionId) throws EntityNotFoundException
+    {
+       
+
+        Optional<RestriccionEntity> restriccionEntity = restriccionRepository.findById(restriccionId);
+
+        if(restriccionEntity.isEmpty())
+            throw new EntityNotFoundException("ACTIVIDAD_NOT_FOUND");
+        
+     
+        return restriccionEntity.get(); 
+    }
+
+    @Transactional
+    public RestriccionEntity createRestriccion(RestriccionEntity restriccionEntity) throws IllegalOperationException {
+        
+        return restriccionRepository.save(restriccionEntity);
+    }
+
+
 }
