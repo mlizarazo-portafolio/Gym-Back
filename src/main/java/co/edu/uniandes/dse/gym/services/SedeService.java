b8f7e4dcac56b39f2579c340dd4ef1b042b95825
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-02-15 09:19:00 -0500

    Sede & Servicio

--- src/main/java/co/edu/uniandes/dse/gym/services/SedeService.java
+++ src/main/java/co/edu/uniandes/dse/gym/services/SedeService.java
@@ -1,8 +1,42 @@
 package co.edu.uniandes.dse.gym.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.uniandes.dse.gym.entities.SedeEntity;
+import co.edu.uniandes.dse.gym.repositories.SedeRepository;
+import co.edu.uniandes.dse.gym.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.gym.exceptions.IllegalOperationException;
+
 
 @Service
 public class SedeService {
     
+    @Autowired
+    SedeRepository sedeRepository;
+
+    @Transactional
+    public SedeEntity createSede(SedeEntity sedeEntity) throws EntityNotFoundException, IllegalOperationException {
+        return sedeRepository.save(sedeEntity);
+    }
+
+    @Transactional
+    public List<SedeEntity> getSedes() {
+        return sedeRepository.findAll();
+    }
+
+    @Transactional
+    public SedeEntity getSedeById(Long sedeId) throws EntityNotFoundException {
+        Optional<SedeEntity> sedeEntity = sedeRepository.findById(sedeId);
+
+        if(sedeEntity.isEmpty()) {
+            throw new EntityNotFoundException("SEDE NOT FOUND");
+        }
+        return sedeEntity.get();
+    }
+
 }
--- src/main/java/co/edu/uniandes/dse/gym/services/ServicioService.java
+++ src/main/java/co/edu/uniandes/dse/gym/services/ServicioService.java
@@ -1,8 +1,42 @@
 package co.edu.uniandes.dse.gym.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.uniandes.dse.gym.entities.ServicioEntity;
+import co.edu.uniandes.dse.gym.repositories.ServicioRepository;
+import co.edu.uniandes.dse.gym.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.gym.exceptions.IllegalOperationException;
+
 
 @Service
 public class ServicioService {
     
+    @Autowired
+    ServicioRepository servicioRepository;
+
+    @Transactional
+    public ServicioEntity createServicio(ServicioEntity servicioEntity) throws EntityNotFoundException, IllegalOperationException {
+        return servicioRepository.save(servicioEntity);
+    }
+
+    @Transactional
+    public List<ServicioEntity> getServicios() {
+        return servicioRepository.findAll();
+    }
+
+    @Transactional
+    public ServicioEntity getServicioById(Long servicioId) throws EntityNotFoundException {
+        Optional<ServicioEntity> servicioEntity = servicioRepository.findById(servicioId);
+
+        if(servicioEntity.isEmpty()) {
+            throw new EntityNotFoundException("SERVICIO NOT FOUND");
+        }
+        return servicioEntity.get();
+    }
+
 }
