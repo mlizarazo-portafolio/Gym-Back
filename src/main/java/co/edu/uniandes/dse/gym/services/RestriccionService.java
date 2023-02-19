8180c4a45c05a147d823b8de100f7803a9dc1cc2
CarlosMunoz0 <c.munoza@uniandes.edu.co>
2023-02-19 15:43:32 -0500

    Logica restriccion

--- src/main/java/co/edu/uniandes/dse/gym/services/RestriccionService.java
+++ src/main/java/co/edu/uniandes/dse/gym/services/RestriccionService.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
 import co.edu.uniandes.dse.gym.entities.RestriccionEntity;
 import co.edu.uniandes.dse.gym.exceptions.EntityNotFoundException;
 import co.edu.uniandes.dse.gym.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.gym.repositories.RestriccionRepository;
 
+
+@Slf4j
 @Service
 public class RestriccionService{
     
@@ -19,32 +22,72 @@ public class RestriccionService{
     RestriccionRepository restriccionRepository;
 
 
+    @Transactional
+    public RestriccionEntity createRestriccion(RestriccionEntity restriccionEntity) throws IllegalOperationException {
+        log.info("Inicia proceso de creación de la restriccion");
+        if(restriccionEntity.getEdad()<0)
+        {
+            throw new IllegalOperationException("Edad no valida");
+        }
 
+        log.info("Termina proceso de creación del libro");
+        return restriccionRepository.save(restriccionEntity);
+    }
+
+    //Obtener todas las restricciones
     @Transactional
     public List<RestriccionEntity> getRestricciones(){
-
+        log.info("Inicia proceso de consultar todas las restricciones");
         return restriccionRepository.findAll();
     }
 
+
+    //Obtener una restriccion
     @Transactional
     public RestriccionEntity getRestriccion(Long restriccionId) throws EntityNotFoundException
     {
        
-
+        log.info("Inicia proceso de consultar la restriccion con id = {0}", restriccionId);
         Optional<RestriccionEntity> restriccionEntity = restriccionRepository.findById(restriccionId);
 
         if(restriccionEntity.isEmpty())
-            throw new EntityNotFoundException("ACTIVIDAD_NOT_FOUND");
-        
-     
+        {
+            throw new EntityNotFoundException("RESTRICCION_NOT_FOUND");
+        }
+
+        log.info("Termina proceso de consultar la restriccion con id = {0}", restriccionId);
         return restriccionEntity.get(); 
     }
 
+    //Actualizar
     @Transactional
-    public RestriccionEntity createRestriccion(RestriccionEntity restriccionEntity) throws IllegalOperationException {
-        
-        return restriccionRepository.save(restriccionEntity);
+    public RestriccionEntity updateRestriccion(Long restriccionId, RestriccionEntity restriccion)  throws EntityNotFoundException
+    {
+        log.info("Inicia proceso de actualizar la restriccion con id = {0}", restriccionId);
+        Optional<RestriccionEntity> restriccionEntity = restriccionRepository.findById(restriccionId);
+        if(restriccionEntity.isEmpty())
+        {
+            throw new EntityNotFoundException("RESTRICCION_NOT_FOUND");
+        }
+
+        restriccion.setId(restriccionId);
+        log.info("Termina proceso de actualizar la restriccion con id = {0}", restriccionId);
+        return restriccionRepository.save(restriccion);
     }
 
+    //Borrar
+    @Transactional
+    public void deleteRestriccion(Long restriccionId) throws EntityNotFoundException
+    {
+        log.info("Inicia proceso de borrar la restriccion con id = {0}", restriccionId);
+        
+        Optional<RestriccionEntity> restriccionEntity = restriccionRepository.findById(restriccionId);
+        if(restriccionEntity.isEmpty())
+        {
+            throw new EntityNotFoundException("RESTRICCION_NOT_FOUND");
+        }
+        restriccionRepository.deleteById(restriccionId);
+        log.info("Termina proceso de borrar la restriccion con id = {0}", restriccionId);
+    }
 
 }
