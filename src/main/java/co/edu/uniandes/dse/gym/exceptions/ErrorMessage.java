abcee3531da33c756ad8dca97c323bbd031b4484
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-02-19 21:34:23 -0500

    Sede, Servicio y ErrorMessage

--- src/main/java/co/edu/uniandes/dse/gym/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/uniandes/dse/gym/exceptions/ErrorMessage.java
@@ -1,8 +1,8 @@
 package co.edu.uniandes.dse.gym.exceptions;
 
 public final class ErrorMessage {
-	public static final String SERVICIO_NOT_FOUND = "";
-	public static final String SEDE_NOT_FOUND = "";
+	public static final String SERVICIO_NOT_FOUND = "No se encontró el servicio";
+	public static final String SEDE_NOT_FOUND = "No se encontró la sede";
 	public static final String RESTRICCION_NOT_FOUND = "";
 	public static final String RESENIA_NOT_FOUND = "";
 	public static final String PLAN_ENTRENAMIENTO_NOT_FOUND = "";
