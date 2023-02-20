47d2dec262610f301fc503a619845b5a8422e6b3
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-02-20 18:58:26 -0500

    SedeServiceTest y ServicioServiceTest

--- src/test/java/co/edu/uniandes/dse/gym/services/SedeServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/gym/services/SedeServiceTest.java
@@ -1,5 +1,34 @@
 package co.edu.uniandes.dse.gym.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import co.edu.uniandes.dse.gym.entities.SedeEntity;
+
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional
+@Import(SedeService.class)
 public class SedeServiceTest {
+    @Autowired
+    private SedeService sedeService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private PodamFactory factory = new PodamFactoryImpl();
     
+    private List<SedeEntity> sedeList = new ArrayList<>();
 }
--- src/test/java/co/edu/uniandes/dse/gym/services/ServicioServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/gym/services/ServicioServiceTest.java
@@ -1,5 +1,34 @@
 package co.edu.uniandes.dse.gym.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import co.edu.uniandes.dse.gym.entities.ServicioEntity;
+
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional
+@Import(ServicioService.class)
 public class ServicioServiceTest {
+    @Autowired
+    private ServicioService ServicioService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private PodamFactory factory = new PodamFactoryImpl();
     
+    private List<ServicioEntity> servicioList = new ArrayList<>();
 }
