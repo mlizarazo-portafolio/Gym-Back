8709af8631c2926fc21973ff536357485ad4000f
SantiTV <s.tenjov@uniandes.edu.co>
2023-02-20 18:26:47 -0500

    cambios PlanEntrenamientoService

--- src/main/java/co/edu/uniandes/dse/gym/services/PlanEntrenamientoService.java
+++ src/main/java/co/edu/uniandes/dse/gym/services/PlanEntrenamientoService.java
@@ -1,8 +1,52 @@
 package co.edu.uniandes.dse.gym.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.apache.logging.log4j.message.Message;
+import org.modelmapper.spi.ErrorMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.gym.entities.PlanEntrenamientoEntity;
+import co.edu.uniandes.dse.gym.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.gym.repositories.PlanEntrenamientoRepository;
 
 @Service
 public class PlanEntrenamientoService {
+
+    @Autowired
+    PlanEntrenamientoRepository planEntrenamientoRepository;
+
+    /**
+     * Obtiene la 
+     * 
+     */
+
+    @Transactional
+    public List<PlanEntrenamientoEntity> getPlanes(){
+        return planEntrenamientoRepository.findAll();
+    }
+
+    @Transactional
+    public PlanEntrenamientoEntity getPlan(Long planId) throws EntityNotFoundException{
+
+        Optional < PlanEntrenamientoEntity > planEntrenamientoEntity = planEntrenamientoRepository.findById(planId);
+
+        if(planEntrenamientoEntity.isEmpty())
+            throw new EntityNotFoundException("PLAN_NOT_FOUND");
     
+    
+        return planEntrenamientoEntity.get();
+    }
+    
+    
+    @Transactional
+    public PlanEntrenamientoEntity crearPlan(PlanEntrenamientoEntity plan){
+        return planEntrenamientoRepository.save(plan);
+    }
+        
 }
+
