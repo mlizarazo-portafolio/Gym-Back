cd0f8c9b25114e3403fe0fe95bc9e679fa6d8632
dgomezrey <d.gomezr2@uniandes.edu.co>
2023-02-20 21:37:25 -0500

    ErrorMessage edit

--- src/main/java/co/edu/uniandes/dse/gym/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/uniandes/dse/gym/exceptions/ErrorMessage.java
@@ -4,9 +4,9 @@ public final class ErrorMessage {
 	public static final String SERVICIO_NOT_FOUND = "";
 	public static final String SEDE_NOT_FOUND = "";
 	public static final String RESTRICCION_NOT_FOUND = "";
-	public static final String RESENIA_NOT_FOUND = "";
+	public static final String RESENIA_NOT_FOUND = "No se encontro la resenia";
 	public static final String PLAN_ENTRENAMIENTO_NOT_FOUND = "";
-	public static final String ENTRENADOR_NOT_FOUND = "";
+	public static final String ENTRENADOR_NOT_FOUND = "No se encontro el entrenador";
     public static final String DEPORTOLOGO_NOT_FOUND = "";
     public static final String CONVENIO_NOT_FOUND = "";
     public static final String ATLETA_NOT_FOUND = "";
