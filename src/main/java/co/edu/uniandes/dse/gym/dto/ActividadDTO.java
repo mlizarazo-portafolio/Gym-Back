fb17ef5b3dc995602f8774a67452234f396a24fc
CarlosMunoz0 <c.munoza@uniandes.edu.co>
2023-02-28 09:00:05 -0500

    actividad y restriccion dto

---
+++ src/main/java/co/edu/uniandes/dse/gym/dto/ActividadDTO.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.dse.gym.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ActividadDTO {
+    private Long id;
+    private String nombre;
+    private Integer maxParticipantes;
+    private String horario;
+    private String tipo;
+}
---
+++ src/main/java/co/edu/uniandes/dse/gym/dto/RestriccionDTO.java
@@ -0,0 +1,12 @@
+package co.edu.uniandes.dse.gym.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestriccionDTO {
+    private Long id;
+    private Integer edad;
+    private String condFisica;
+}
