59da68772f277a8223b270d60d5e6d71341573b4
Maria <m.lizarazop@uniandes.edu.co>
2023-02-28 09:06:19 -0500

    detailDTO atleta y DeportologoDTO

--- src/main/java/co/edu/uniandes/dse/gym/dto/DeportologoDTO.java
+++ src/main/java/co/edu/uniandes/dse/gym/dto/DeportologoDTO.java
@@ -1,5 +1,11 @@
 package co.edu.uniandes.dse.gym.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
 public class DeportologoDTO {
     private String nombre;
     private String login;
