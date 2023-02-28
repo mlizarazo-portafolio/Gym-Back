9135aa5bfca1a306d7f113ad6ed176e954db1c6b
SantiTV <79552393+SantiTV@users.noreply.github.com>
2023-02-28 09:09:55 -0500

    detail Convenio y Plan

---
+++ src/main/java/co/edu/uniandes/dse/gym/dto/PlanEntrenamientoDetailDTO.java
@@ -0,0 +1,20 @@
+package co.edu.uniandes.dse.gym.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+
+
+public class PlanEntrenamientoDetailDTO extends PlanEntrenamientoDTO{
+
+    private String objetivoBasico;
+    private String nombre;
+    private String descripcion;
+    private String dirrecion;
+    private String coordenada;
+    private Integer duracion;
+    private Integer costo;
+
+}
