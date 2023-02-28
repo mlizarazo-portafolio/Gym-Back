26ca9add2cd93555c3d835d32a3355d2908d67c7
dgomezrey <d.gomezr2@uniandes.edu.co>
2023-02-28 08:48:30 -0500

    DTO resenia y entrenador

---
+++ src/main/java/co/edu/uniandes/dse/gym/dto/EntrenadorDTO.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.dse.gym.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EntrenadorDTO {
+
+    private Long id;
+    private String nombre;
+    private String foto;
+    private String trayectoria;
+    
+}
