610574b55d0feff50636380db058b9c40a42520a
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-02-28 08:46:48 -0500

    DTO Sede y Servicio

---
+++ src/main/java/co/edu/uniandes/dse/gym/dto/SedeDTO.java
@@ -0,0 +1,13 @@
+package co.edu.uniandes.dse.gym.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SedeDTO {
+    private Long id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+}
