d788730e4f24d2232cd41b5ccd2887816a6df787
Maria <m.lizarazop@uniandes.edu.co>
2023-03-29 10:07:57 -0500

    hksd

--- src/main/java/co/edu/uniandes/dse/gym/entities/PlanEntrenamientoEntity.java
+++ src/main/java/co/edu/uniandes/dse/gym/entities/PlanEntrenamientoEntity.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 import lombok.Setter;
 import uk.co.jemos.podam.common.PodamExclude;
 
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -17,23 +16,19 @@ import java.util.ArrayList;
 @Setter
 @Entity
 
-public class PlanEntrenamientoEntity extends  BaseEntity{
+public class PlanEntrenamientoEntity extends BaseEntity {
 
-@PodamExclude
-@ManyToMany
-private List<SedeEntity> sedes = new ArrayList<>();
+    @PodamExclude
+    @ManyToMany
+    private List<SedeEntity> sedes = new ArrayList<>();
 
-@PodamExclude
-@OneToMany (mappedBy = "plan",  cascade = CascadeType.PERSIST, orphanRemoval = true)
-private List<ConvenioEntity> convenios = new ArrayList<>();
-  
+    @PodamExclude
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<ConvenioEntity> convenios = new ArrayList<>();
 
-@PodamExclude
-@OneToMany (mappedBy = "plan", cascade = CascadeType.PERSIST, orphanRemoval = true)
-private List<AtletaEntity> atletasInscritos = new ArrayList<>();
- 
- 
- 
+    @PodamExclude
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<AtletaEntity> atletasInscritos = new ArrayList<>();
 
     private Long id;
     private String nombre;
@@ -42,8 +37,8 @@ private List<AtletaEntity> atletasInscritos = new ArrayList<>();
     private String direccion;
     private Integer duracion;
     private Integer costo;
-    
-    
+
+    public void setConvenios(ConvenioEntity convenioEntity) {
+    }
 
 }
-
