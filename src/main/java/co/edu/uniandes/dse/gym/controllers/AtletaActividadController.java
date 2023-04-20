e759097a4a1db1ba1614a1282330bd93790a1958
CarlosMunoz0 <c.munoza@uniandes.edu.co>
2023-04-19 22:33:23 -0500

    AtletaActividad

---
+++ src/main/java/co/edu/uniandes/dse/gym/controllers/AtletaActividadController.java
@@ -0,0 +1,77 @@
+package co.edu.uniandes.dse.gym.controllers;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uniandes.dse.gym.dto.ActividadDTO;
+import co.edu.uniandes.dse.gym.dto.ActividadDetailDTO;
+import co.edu.uniandes.dse.gym.entities.ActividadEntity;
+import co.edu.uniandes.dse.gym.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.gym.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.gym.services.AtletaActividadService;
+
+@RestController
+@RequestMapping("/atletas")
+public class AtletaActividadController {
+    @Autowired
+    private AtletaActividadService atletaActividadService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping(value = "/{atletaId}/actividades/{actividadId}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public ActividadDetailDTO getActividad(@PathVariable("atletaId") Long atletaId, @PathVariable("actividadId") Long actividadId)
+			throws EntityNotFoundException, IllegalOperationException {
+		ActividadEntity actividadEntity = atletaActividadService.getActividad(atletaId, actividadId);
+		return modelMapper.map(actividadEntity, ActividadDetailDTO.class);
+	}
+
+    @GetMapping(value = "/{atletaId}/actividades")
+	@ResponseStatus(code = HttpStatus.OK)
+	public List<ActividadDetailDTO> getActividades(@PathVariable("atletaId") Long atletaId) throws EntityNotFoundException {
+		List<ActividadEntity> actividadEntity = atletaActividadService.getActividades(atletaId);
+		return modelMapper.map(actividadEntity, new TypeToken<List<ActividadDetailDTO>>() {
+		}.getType());
+	}
+
+    @PostMapping(value = "/{atletaId}/actividades/{actividadId}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public ActividadDetailDTO addActividad(@PathVariable("atletaId") Long atletaId, @PathVariable("actividadId") Long actividadId)
+			throws EntityNotFoundException {
+		ActividadEntity actividadEntity = atletaActividadService.addActividad(atletaId, actividadId);
+		return modelMapper.map(actividadEntity, ActividadDetailDTO.class);
+	}
+
+    @PutMapping(value = "/{atletaId}/actividades")
+	@ResponseStatus(code = HttpStatus.OK)
+	public List<ActividadDetailDTO> replaceActividades(@PathVariable("atletaId") Long atletaId, @RequestBody List<ActividadDTO> actividades)
+			throws EntityNotFoundException {
+		List<ActividadEntity> entities = modelMapper.map(actividades, new TypeToken<List<ActividadEntity>>() {
+		}.getType());
+		List<ActividadEntity> actividadesList = atletaActividadService.addActividades(atletaId, entities);
+		return modelMapper.map(actividadesList, new TypeToken<List<ActividadDetailDTO>>() {
+		}.getType());
+
+	}
+
+    @DeleteMapping(value = "/{atletaId}/actividades/{actividadId}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void removeActividad(@PathVariable("atletaId") Long atletaId, @PathVariable("actividadId") Long actividadId)
+			throws EntityNotFoundException {
+		atletaActividadService.removeActividad(atletaId, actividadId);
+	}
+}
--- src/main/java/co/edu/uniandes/dse/gym/services/AtletaActividadService.java
+++ src/main/java/co/edu/uniandes/dse/gym/services/AtletaActividadService.java
@@ -29,27 +29,27 @@ public class AtletaActividadService {
 
     @Transactional
 	public ActividadEntity addActividad(Long atletaId, Long actividadId) throws EntityNotFoundException {
-		log.info("Inicia proceso de asociarle una Actividad a la atleta con id = {0}", atletaId);
+		log.info("Inicia proceso de asociarle una Actividad a el atleta con id = {0}", atletaId);
 		Optional<AtletaEntity> atletaEntity = atletaRepository.findById(atletaId);
 		Optional<ActividadEntity> actividadEntity = actividadRepository.findById(actividadId);
 
 		if (atletaEntity.isEmpty())
-			throw new EntityNotFoundException(ErrorMessage.SEDE_NOT_FOUND);
+			throw new EntityNotFoundException(ErrorMessage.ATLETA_NOT_FOUND);
 
 		if (actividadEntity.isEmpty())
 			throw new EntityNotFoundException(ErrorMessage.ACTIVIDAD_NOT_FOUND);
 
 		actividadEntity.get().getAtletas().add(atletaEntity.get());
-		log.info("Termina proceso de asociarle una Actividad a la atleta con id = {0}", atletaId);
+		log.info("Termina proceso de asociarle una Actividad a el atleta con id = {0}", atletaId);
 		return actividadEntity.get();
 	}
 
     @Transactional
 	public List<ActividadEntity> getActividades(Long atletaId) throws EntityNotFoundException {
-		log.info("Inicia proceso de consultar todas las actividades de la atleta con id = {0}", atletaId);
+		log.info("Inicia proceso de consultar todas las actividades de el atleta con id = {0}", atletaId);
 		Optional<AtletaEntity> atletaEntity = atletaRepository.findById(atletaId);
 		if (atletaEntity.isEmpty())
-			throw new EntityNotFoundException(ErrorMessage.SEDE_NOT_FOUND);
+			throw new EntityNotFoundException(ErrorMessage.ATLETA_NOT_FOUND);
 
 		log.info("Termina proceso de consultar todas las actividades de la atleta con id = {0}", atletaId);
 		return atletaEntity.get().getActividades();
@@ -57,29 +57,29 @@ public class AtletaActividadService {
 
     @Transactional
 	public ActividadEntity getActividad(Long atletaId, Long actividadId) throws EntityNotFoundException, IllegalOperationException {
-		log.info("Inicia proceso de consultar la actividad con id = {0} de la atleta con id = " + atletaId, actividadId);
+		log.info("Inicia proceso de consultar la actividad con id = {0} de el atleta con id = " + atletaId, actividadId);
 		Optional<AtletaEntity> atletaEntity = atletaRepository.findById(atletaId);
 		Optional<ActividadEntity> actividadEntity = actividadRepository.findById(actividadId);
 
 		if (atletaEntity.isEmpty())
-			throw new EntityNotFoundException(ErrorMessage.SEDE_NOT_FOUND);
+			throw new EntityNotFoundException(ErrorMessage.ATLETA_NOT_FOUND);
 
 		if (actividadEntity.isEmpty())
 			throw new EntityNotFoundException(ErrorMessage.ACTIVIDAD_NOT_FOUND);
 
-		log.info("Termina proceso de consultar la actividad con id = {0} de la atleta con id = " + atletaId, actividadId);
+		log.info("Termina proceso de consultar la actividad con id = {0} de el atleta con id = " + atletaId, actividadId);
 		if (!actividadEntity.get().getAtletas().contains(atletaEntity.get()))
-			throw new IllegalOperationException("La actividad no esta asociada con la atleta");
+			throw new IllegalOperationException("La actividad no esta asociada con el atleta");
 		
 		return actividadEntity.get();
 	}
 
     @Transactional
 	public List<ActividadEntity> addActividades(Long atletaId, List<ActividadEntity> actividades) throws EntityNotFoundException {
-		log.info("Inicia proceso de reemplazar las actividades asociadas a la atleta con id = {0}", atletaId);
+		log.info("Inicia proceso de reemplazar las actividades asociadas a el atleta con id = {0}", atletaId);
 		Optional<AtletaEntity> atletaEntity = atletaRepository.findById(atletaId);
 		if (atletaEntity.isEmpty())
-			throw new EntityNotFoundException(ErrorMessage.SEDE_NOT_FOUND);
+			throw new EntityNotFoundException(ErrorMessage.ATLETA_NOT_FOUND);
 
 		for (ActividadEntity actividad : actividades) {
 			Optional<ActividadEntity> actividadEntity = actividadRepository.findById(actividad.getId());
@@ -89,17 +89,17 @@ public class AtletaActividadService {
 			if (!actividadEntity.get().getAtletas().contains(atletaEntity.get()))
 				actividadEntity.get().getAtletas().add(atletaEntity.get());
 		}
-		log.info("Finaliza proceso de reemplazar las actividades asociadas a la atleta con id = {0}", atletaId);
+		log.info("Finaliza proceso de reemplazar las actividades asociadas a el atleta con id = {0}", atletaId);
 		atletaEntity.get().setActividades(actividades);
 		return atletaEntity.get().getActividades();
 	}
 
     @Transactional
 	public void removeActividad(Long atletaId, Long actividadId) throws EntityNotFoundException {
-		log.info("Inicia proceso de borrar una actividad de la atleta con id = {0}", atletaId);
+		log.info("Inicia proceso de borrar una actividad de el atleta con id = {0}", atletaId);
 		Optional<AtletaEntity> atletaEntity = atletaRepository.findById(atletaId);
 		if (atletaEntity.isEmpty())
-			throw new EntityNotFoundException(ErrorMessage.SEDE_NOT_FOUND);
+			throw new EntityNotFoundException(ErrorMessage.ATLETA_NOT_FOUND);
 
 		Optional<ActividadEntity> actividadEntity = actividadRepository.findById(actividadId);
 		if (actividadEntity.isEmpty())
@@ -107,6 +107,6 @@ public class AtletaActividadService {
 
 		actividadEntity.get().getAtletas().remove(atletaEntity.get());
 		atletaEntity.get().getActividades().remove(actividadEntity.get());
-		log.info("Finaliza proceso de borrar una actividad de la atleta con id = {0}", atletaId);
+		log.info("Finaliza proceso de borrar una actividad de el atleta con id = {0}", atletaId);
 	}
 }
