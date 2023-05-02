8a2401429a9b14c047a976cac75a24b1f831afc0
CarlosMunoz0 <c.munoza@uniandes.edu.co>
2023-05-02 08:38:13 -0500

    sql actividad

--- sql/data.sql
+++ sql/data.sql
@@ -1,3 +1,4 @@
 insert into ENTRENADOR_ENTITY (id, nombre, foto, trayectoria) values (1, 'Messi', 'http://dummyimage.com/241x100.png/5fa2dd/ffffff', 'newells, fc barcelona, psg');
 insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (1, 'Central', 'Calle 31 Norte #7-21', '7788918');
 insert into PLANENTRENAMIENTO_ENTITY(id, nombre, objetivoBasico, descripcion, direccion, duracion, costo) values(3, "Fuerza I", "Aumento de fuerza", "Ejercicio basado en fuerza","Cra7",3,3000);
+insert into ACTIVIDAD_ENTITY(id, nombre, maxParticipantes, horario, tipo, entrenador) values (1, 'Yoga', 30, 'Lunes - 12:30 a 2:30', 1);
