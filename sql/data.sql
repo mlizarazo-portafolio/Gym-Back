cdc041b9e8330f8b6050f87230bd421a99e690f4
SantiTV <79552393+SantiTV@users.noreply.github.com>
2023-05-02 08:23:37 -0500

    sql planEntrenamiento

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,2 @@
 insert into ENTRENADOR_ENTITY (id, nombre, foto, trayectoria) values (1, 'Messi', 'imagen.com', 'newells, fc barcelona, psg');
+insert into PLANENTRENAMIENTO_ENTITY(id, nombre, objetivoBasico, descripcion, direccion, duracion, costo) values(3, "Fuerza I", "Aumento de fuerza", "Ejercicio basado en fuerza","Cra7",3,3000);
