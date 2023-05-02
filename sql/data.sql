d7dbdc4857b5f51024899d9f9565d1e91a8cc796
SantiTV <79552393+SantiTV@users.noreply.github.com>
2023-05-02 08:50:24 -0500

    data correccion

--- sql/data.sql
+++ sql/data.sql
@@ -1,3 +1,3 @@
 insert into ENTRENADOR_ENTITY (id, nombre, foto, trayectoria) values (1, 'Messi', 'http://dummyimage.com/241x100.png/5fa2dd/ffffff', 'newells, fc barcelona, psg');
 insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (1, 'Central', 'Calle 31 Norte #7-21', '7788918');
-insert into PLANENTRENAMIENTO_ENTITY(id, nombre, objetivoBasico, descripcion, direccion, duracion, costo) values(3, "Fuerza I", "Aumento de fuerza", "Ejercicio basado en fuerza","Cra7",3,3000);
+insert into PLAN_ENTRENAMIENTO_ENTITY(id, nombre, objetivo_Basico, descripcion, direccion, duracion, costo) values(3, 'Fuerza I', 'Aumento de fuerza', 'Ejercicio basado en fuerza','Cra7',3,3000);
