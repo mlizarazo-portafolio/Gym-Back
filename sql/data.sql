dadaa0e75c1b711545ab31fbfdae3fb36879e3a3
CarlosMunoz0 <c.munoza@uniandes.edu.co>
2023-05-03 10:24:14 -0500

    sql actividad y restriccion

--- sql/data.sql
+++ sql/data.sql
@@ -2,4 +2,5 @@ insert into ENTRENADOR_ENTITY (id, nombre, foto, trayectoria) values (1, 'Messi'
 insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (1, 'Central', 'Calle 31 Norte #7-21', '7788918');
 insert into SERVICIO_ENTITY (id, servicio, disponible, sede_id) values (1, 'Pesas', True, 1);
 insert into PLAN_ENTRENAMIENTO_ENTITY(id, nombre, objetivo_Basico, descripcion, direccion, duracion, costo) values(3, 'Fuerza I', 'Aumento de fuerza', 'Ejercicio basado en fuerza','Cra7',3,3000);
-insert into ACTIVIDAD_ENTITY(id, nombre, max_Participantes, horario, tipo, entrenador_id) values (1, 'Yoga', 30, 'Lunes - 12:30 a 2:30', 1);
+insert into ACTIVIDAD_ENTITY(id, nombre, max_Participantes, horario, tipo) values (1, 'Yoga', 30, 'Lunes - 12:30 a 2:30', 'Grupal');
+insert into RESTRICCION_ENTITY (id, edad, cond_fisica, actividad_id) values (1, 45, 'buena resistencia', 1);
