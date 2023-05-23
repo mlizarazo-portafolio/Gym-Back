aaeb18ae19a2a9caa840cfe98f712ead39acd0a9
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-05-23 09:00:46 -0500

    Pirata con Radio 4

--- sql/data.sql
+++ sql/data.sql
@@ -56,11 +56,11 @@ insert into RESENIA_ENTITY(id, usuario, puntuacion, comentario, sede_id) values 
 insert into RESENIA_ENTITY(id, usuario, puntuacion, comentario, sede_id) values (3, 'jorge1', 5, 'Messi debería venir es muy bueno.', 2);
 insert into RESENIA_ENTITY(id, usuario, puntuacion, comentario, sede_id) values (4, 'alberto69', 0, 'Mu malo no me lleven otra vez!', 2);
 
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (1, 1)
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (2, 2)
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (3, 3)
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (4, 4)
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (5, 5)
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (6, 6)
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (7, 7)
-insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (8, 8)
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (1, 1);
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (2, 2);
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (3, 3);
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (4, 4);
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (5, 5);
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (6, 6);
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (7, 7);
+insert into ACTIVIDAD_ENTITY_SEDES(actividades_id, sedes_id) values (8, 8);
