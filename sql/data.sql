457e8a203af3ff91973199aeadd94b3751e48231
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-05-23 08:43:10 -0500

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -3,12 +3,23 @@ insert into ENTRENADOR_ENTITY (id, nombre, foto, trayectoria) values (2, 'El bic
 insert into ENTRENADOR_ENTITY (id, nombre, foto, trayectoria) values (3, 'Marco Mbapperez', 'https://www.elmundo.com/assets/uploads/files/36586-vladimir-hernandez.jpg', 'Junior');
 
 insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (1, 'Central', 'Calle 31 Norte #7-21', '7788918');
-insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (2, 'Occidental', 'Calle 15 Oeste #3-81', '9988112');
+insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (2, 'Occidental', 'Calle 15 Oeste #3-49', '9988112');
+insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (3, 'Pasoancho', 'Calle 14 Sur #1-23', '7208698');
+insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (4, 'Pance', 'Calle 16 Este #9-92', '3105288');
+insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (5, 'Guajira', 'Calle 29 Norte #6-42', '8152054');
+insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (6, 'Amazonas', 'Calle 1 Oeste #2-81', '9904768');
+insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (7, 'Arauca', 'Calle 3 Sur #1-33', '7406985');
+insert into SEDE_ENTITY (id, nombre, direccion, telefono) values (8, 'Guaviare', 'Calle 7 Este #32-47', '2898560');
+
 insert into SERVICIO_ENTITY (id, servicio, disponible, sede_id) values (1, 'Pesas', True, 1);
 insert into SERVICIO_ENTITY (id, servicio, disponible, sede_id) values (2, 'Masajes', True, 2);
+
 insert into PLAN_ENTRENAMIENTO_ENTITY(id, nombre, objetivo_Basico, descripcion, direccion, duracion, costo) values(3, 'Fuerza I', 'Aumento de fuerza', 'Ejercicio basado en fuerza','Cra7',3,3000);
+
 insert into ACTIVIDAD_ENTITY(id, nombre, max_Participantes, horario, tipo, entrenador_id) values (1, 'Yoga', 30, 'Lunes - 12:30 a 2:30', 'Grupal', 1);
+
 insert into RESTRICCION_ENTITY (id, edad, cond_fisica, actividad_id) values (1, 45, 'buena resistencia', 1);
+
 insert into CONVENIO_ENTITY(id, nombre,descuento) values(1,'Compensar', 10);
 
 insert into RESENIA_ENTITY(id, usuario, puntuacion, comentario, sede_id) values (1, 'dgomezrey', 4, 'Es el mejor gimnasio al que he ido en toda mi vida, excelente servicio. Por favor saquenme de Colombia.', 1);
